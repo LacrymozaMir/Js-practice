69d3fef3de361b3c1a194e848939a62c5a62de47
Danila <easyezyes@gmail.com>
2024-07-11 14:05:59 +0300

    feat(Exercise 12): completed 12 task

---
+++ 12/scripts.js
@@ -0,0 +1,20 @@
+// Напишите функцию JavaScript для добавления строк в таблицу.
+
+const table = document.getElementById('sampleTable');
+let flag = 3;
+
+
+function insert_Row() {
+  const tdFirst = document.createElement('td');
+  const tdSecond = document.createElement('td');
+  const tr = document.createElement('tr');
+  tdFirst.innerHTML = `Row${flag} cell${flag}`
+  tdSecond.innerHTML = `Row${flag} cell${flag}`
+
+  tr.appendChild(tdFirst);
+  tr.appendChild(tdSecond);
+
+  table.appendChild(tr);
+
+  flag++;
+}
